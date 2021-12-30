042a610d72acf571a9e5db215e684f2422f0a7e7
noeulnight <noeulnight.mail@gmail.com>
2021-12-30 09:10:48 +0900

    feat: zero

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,8 @@
-import Head from 'next/head'
 import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
-import { Toaster } from 'react-hot-toast'
-import { CookiesProvider } from 'react-cookie'
 
-function Dormitory ({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Dormitory</title>
-      </Head>
-      <Toaster position='top-right' />
-      <CookiesProvider>
-        <Component {...pageProps} />
-      </CookiesProvider>
-    </>
-  )
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-export default Dormitory
+
+export default MyApp
